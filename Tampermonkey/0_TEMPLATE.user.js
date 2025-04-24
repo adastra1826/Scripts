55a2cb8775c199ea364af5e057e11a58533b686d
Nicholas Doherty <nicholasdoherty95@gmail.com>
2025-04-24 14:33:02 -0400

    Actually add content to template script

---
+++ Tampermonkey/0_TEMPLATE.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-24
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/adastra1826/Scripts/tree/main/Tampermonkey
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
