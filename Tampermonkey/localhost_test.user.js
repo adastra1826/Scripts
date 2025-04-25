6c62ef5514da7852a7a12fd5e96de3c76ae3cb15
Nicholas Doherty <nicholasdoherty95@gmail.com>
2025-04-25 16:33:20 -0400

    Iterate userscript

--- Tampermonkey/localhost_test.user.js
+++ Tampermonkey/localhost_test.user.js
@@ -1,20 +1,43 @@
+// ==UserScript==
+// @name         Localhost Test
+// @author       Nicholas Doherty
+// @namespace    http://tampermonkey.net/
+// @copyright    CC0
+// @version      2025-04-24.2
+// @description  Test the ability to connect to localhost. https://www.tampermonkey.net/documentation.php
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @match        *://*/*
+// @connect      127.0.0.1
+// @run-at       document-idle
+// @downloadURL  https://raw.githubusercontent.com/adastra1826/Scripts/refs/heads/main/Tampermonkey/localhost_test.user.js
+// @updateURL    https://raw.githubusercontent.com/adastra1826/Scripts/refs/heads/main/Tampermonkey/localhost_test.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('Beginning script.')
+
+    const url = 'https://127.0.0.1:4000/dat';
+    const data = JSON.stringify({ test: 'ok' });
+
+    // — fetch() version —
+    fetch(url, {
+        method:  'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body:    data,
+    })
+        .then(response => {
+        console.log('fetch status:', response.status);
+        return response.text();
+    })
+        .then(body => {
+        console.log('fetch body:', body);
+    })
+        .catch(err => {
+        console.error('fetch error:', err);
+    });
 
 
-console.log('fetch');
-
-// — fetch() version —
-fetch(url, {
-    method:  'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body:    data,
-})
-    .then(response => {
-    console.log('fetch status:', response.status);
-    return response.text();
-})
-    .then(body => {
-    console.log('fetch body:', body);
-})
-    .catch(err => {
-    console.error('fetch error:', err);
-});
+    console.log('Ending script.');
+})();
