97e3be671381f796b8d2064e3949c9a0d7d6e208
Nicholas Doherty <nicholasdoherty95@gmail.com>
2025-04-27 23:41:13 -0400

    Iterate userscript. [Auto generated commit message]

--- Tampermonkey/dat_get_instructions.user.js
+++ Tampermonkey/dat_get_instructions.user.js
@@ -3,7 +3,7 @@
 // @author       Nicholas Doherty
 // @namespace    http://tampermonkey.net/
 // @copyright    CC0
-// @version      1.0.2
+// @version      1.0.3
 // @description  Pull data from page and send to localhost. https://www.tampermonkey.net/documentation.php
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @match        *://*/*
@@ -20,7 +20,9 @@
 
     const url = 'https://127.0.0.1:4000/dat';
 
-    const instructionsDOMElement = document.getElementById('instructions');
+    const query = '#task-response\/WorkerTaskResponseView-hybrid-root > div.tw-pointer-events-auto > div > div > div > div.tw-sticky.tw-top-px.tw-h-screen > div > div:nth-child(1) > div > div > div.tw-sticky.tw--top-2.tw--mx-5.tw--mt-2.tw-mb-2.tw-rounded-lg.tw-bg-\[\#F4F4F4\] > div'
+
+    const instructionsDOMElement = document.querySelector(query);
     const instructionsText = instructionsDOMElement.textContent;
     console.log('Text: ', instructionsText);
 
